01da8a7b52e1351ada91a6cf6ee3a698cfbbeb75
Mohammad Sapan <mscsapan@gmail.com>
2022-08-15 22:29:19 +0600

    Splash screen added

--- lib/src/screens/welcome_screen.dart
+++ lib/src/screens/welcome_screen.dart
@@ -1,10 +1,52 @@
+import 'package:daily_shopping/src/constants/constants.dart';
+import 'package:daily_shopping/src/views/custom_clipper.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    final _size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        children: [
+          ClipPath(
+            clipper: PictureClipper(),
+            child: Container(
+              height: _size.height * 0.6,
+              width: _size.width,
+              color: background,
+              child: Image.asset(
+                'assets/woman_vegetable.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: _size.height * 0.4,
+            width: _size.width,
+            child: Column(
+              children: [
+                Text(
+                  'Buy Groceries Easily With us',
+                  style: GoogleFonts.poppins(
+                      fontSize: 36.0, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Listen Podcast and open your world with this application',
+                  style: GoogleFonts.poppins(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.grey,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/src/views/custom_clipper.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PictureClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width / 2.0, size.height - 50.0, size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+}
